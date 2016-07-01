08a6aadfc125814b6295bf594987c49d0d1a673d
Erikson Oliveira <eriksonmagno6@hotmail.com>
2016-06-30 22:25:26 -0300

    Change order of functions and add comments

--- script_localWeather.js
+++ script_localWeather.js
@@ -8,66 +8,7 @@ function location(position){
 */
 $(document).ready(function() {
 
-  //list of weather icons
-   var iconsList = {
-    "01d" : "wi wi-day-sunny",
-    "02d" : "wi wi-day-cloudy",
-    "03d" : "wi wi-cloud",
-    "04d" : "wi wi-cloudy",
-    "09d" : "wi wi-showers",
-    "10d" : "wi wi-day-rain",
-    "11d" : "wi wi-thunderstorm",
-    "13d" : "wi wi-snow",
-    "50d" : "wi wi-fog",
 
-    "01n" : "wi wi-night-clear",
-    "02n" : "wi wi-night-alt-cloudy",
-    "03n" : "wi wi-cloud",
-    "04n" : "wi wi-cloudy",
-    "09n" : "wi wi-showers",
-    "10n" : "wi wi-night-alt-rain",
-    "11n" : "wi wi-thunderstorm",
-    "13n" : "wi wi-snow",
-    "50n" : "wi wi-fog"
-  }
-
-  //Capitalizes the fist letters of the weather description
-  function uppercase(str) {
-    str = str.split(' ');
-    for (var i = 0; i < str.length; i++){
-      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-    }
-    str = str.join(' ');
-    return str;
-  }
-
-  //Calculates temperature in degrees
-      function calcTempC(tempK) {
-        return Math.round(tempK - 273.15);
-      }
-      
-      function calcTempF(tempK) {
-        return Math.round((tempK *9/5) - 459.67);
-      }
-
-  //toggles between celsius and Fahrenheit
-  var unitSwitch = function(temp, tempC, tempF, tC, tF){
-    $(tempF).on('click', function() {
-      if ($(tempC).hasClass("selected")) {
-        $(temp).html(tF);
-        $(tempC).removeClass("selected").addClass("unselected");
-        $(tempF).removeClass("unselected").addClass("selected");
-      }
-    });
-
-    $(tempC).on('click', function() {
-      if ($(tempF).hasClass("selected")) {
-        $(temp).html(tC);
-        $(tempF).removeClass("selected").addClass("unselected");
-        $(tempC).removeClass("unselected").addClass("selected");
-      }
-    });
-  }
 
   //Calls ip-api to get user's location
   $.getJSON("http://ip-api.com/json", function(geop) {
@@ -110,7 +51,7 @@ $(document).ready(function() {
     
     });
     
-    //Calls openweather for forecast data
+    //Call openweather API for forecast data
     $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + latitude + '&lon=' + longitude +'&cnt=3&APPID=9ad8257fe3d7737f364b3b1ea8e7cc53', function(forecWeather) {
       
       //console.log(forecWeather);
@@ -141,31 +82,35 @@ $(document).ready(function() {
         });
       }
 
+      //insert forecast data on the page
+      //High
       $.each($('.tMax'), function(index, value) {                  
         $(this).html(tMaxCelsius(index));
       });
 
       unitForecSwitch('.tMax', '#tempC', '#tempF', tMaxCelsius, tMaxFahrenheit);
 
-
+      //Low
       $.each($('.tMin'), function(index, value) {
         $(this).html(tMinCelsius(index));
       });
     
       unitForecSwitch('.tMin', '#tempC', '#tempF', tMinCelsius, tMinFahrenheit);
 
+      //weather condition
       $.each($(".iconF"), function(index, value) { 
         var forecIcon = forecWeather.list[index].weather[0].icon;
         $(this).addClass(iconsList[forecIcon]);
       });
       
+      //Condition description
       $.each($(".condF"), function(index, value) { 
         var forecDescr = forecWeather.list[index].weather[0].description;
         forecDescr = uppercase(forecDescr);
         $(this).html(forecDescr);
       });
       
-      //writes the days of the week for the forecast
+      //write the days of the week for the forecast
       var d = new Date();
       var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
@@ -197,3 +142,66 @@ $(document).ready(function() {
   });
   
 });
+
+  //WEATHER ICONS LIST AND FUNCTIONS
+
+  //list of weather icons
+   var iconsList = {
+    "01d" : "wi wi-day-sunny",
+    "02d" : "wi wi-day-cloudy",
+    "03d" : "wi wi-cloud",
+    "04d" : "wi wi-cloudy",
+    "09d" : "wi wi-showers",
+    "10d" : "wi wi-day-rain",
+    "11d" : "wi wi-thunderstorm",
+    "13d" : "wi wi-snow",
+    "50d" : "wi wi-fog",
+
+    "01n" : "wi wi-night-clear",
+    "02n" : "wi wi-night-alt-cloudy",
+    "03n" : "wi wi-cloud",
+    "04n" : "wi wi-cloudy",
+    "09n" : "wi wi-showers",
+    "10n" : "wi wi-night-alt-rain",
+    "11n" : "wi wi-thunderstorm",
+    "13n" : "wi wi-snow",
+    "50n" : "wi wi-fog"
+  }
+
+  //Capitalize the fist letters of the weather description
+  function uppercase(str) {
+    str = str.split(' ');
+    for (var i = 0; i < str.length; i++){
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    str = str.join(' ');
+    return str;
+  }
+
+  //Calculate temperature in degrees
+  function calcTempC(tempK) {
+    return Math.round(tempK - 273.15);
+  }
+
+  function calcTempF(tempK) {
+    return Math.round((tempK *9/5) - 459.67);
+  }
+
+  //toggles between celsius and Fahrenheit
+  var unitSwitch = function(temp, tempC, tempF, tC, tF){
+    $(tempF).on('click', function() {
+      if ($(tempC).hasClass("selected")) {
+        $(temp).html(tF);
+        $(tempC).removeClass("selected").addClass("unselected");
+        $(tempF).removeClass("unselected").addClass("selected");
+      }
+    });
+
+    $(tempC).on('click', function() {
+      if ($(tempF).hasClass("selected")) {
+        $(temp).html(tC);
+        $(tempF).removeClass("selected").addClass("unselected");
+        $(tempC).removeClass("unselected").addClass("selected");
+      }
+    });
+  }
