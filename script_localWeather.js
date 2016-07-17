742f449e9e08eac1726d99fb5cd0ac3effd269b2
Erikson Oliveira <eriksonmagno6@hotmail.com>
2016-07-17 20:41:02 -0300

    Begin implement bckground change;  improve css_sm

--- script_localWeather.js
+++ script_localWeather.js
@@ -9,7 +9,7 @@ function location(position){
 
 $(document).ready(function() {
 
-  //Call ip-api to get user's location
+  /***Call ip-api to get user's location***/
   $.getJSON("http://ip-api.com/json", function(geop) {
 
     var latitude = geop.lat;
@@ -19,21 +19,23 @@ $(document).ready(function() {
     var locationName = $("<span>" + city + ", " + country + "</span>");
     //console.log(latitude + ' ' + longitude);
 
-    //call openweathermap to get current weather information
+    /***call openweathermap to get current weather information***/
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=9ad8257fe3d7737f364b3b1ea8e7cc53', function(currWeather) {
       //console.log(currWeather);
 
-      //Set the weather variables from the JSON data
+      /***Set the weather variables from the JSON data***/
       var tCelsius = "<span>" + calcTempC(currWeather.main.temp_max) + "</span>";
       var tFahrenheit = "<span>" + Math.round(calcTempF(currWeather.main.temp_max)) + "</span>";
 
       var weatherIcon = currWeather.weather[0].icon;
+      var backgroundCol = "a" + weatherIcon.toString();
+      console.log(backgroundCol);
       var description = currWeather.weather[0].description;
       var windsp = Math.round(currWeather.wind.speed);
       var winddir = Math.round(currWeather.wind.deg);
       var humidity = currWeather.main.humidity;
 
-      //add to the page the city and country names and current weather for the user's location
+      /***add to the page the city and country names and current weather for the user's location***/
       $("#cityName").html(locationName);
       $("#temp").html(tCelsius);
       $(".degrees").addClass("wi wi-degrees");
@@ -41,25 +43,28 @@ $(document).ready(function() {
       $("#wind").html("Wind <i class=\"wi wi-wind from-" + winddir + "-deg\"></i>  " + windsp + " m/s");
       $("#RU").append("<i class=\"wi wi-humidity\"></i> Humidity " + humidity + "%");
 
-      //add icon and weather description to the page
-      $("#icon").addClass(iconsList[weatherIcon]);    
+      /***add weather icon and description to the page and change backgound***/
+      //icon
+      $("#icon").addClass(iconsList[weatherIcon]);
       //condition text
       description = uppercase(description);      
-      $("#condition").html(description);  
+      $("#condition").html(description);
+      //background
+      $("#main, .forecBox").addClass(backgroundCol);
 
 
-      //Call openweather API for forecast data
+      /***Call openweather API for forecast data***/
       $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + latitude + '&lon=' + longitude +'&cnt=3&APPID=9ad8257fe3d7737f364b3b1ea8e7cc53', function(forecWeather) {
         //console.log(forecWeather);
 
-        //calculate temp in Celsius and Fahrenheit for forecast data
+      /***calculate temp in Celsius and Fahrenheit for forecast data***/
       function tMaxCelsius(j) { return calcTempC(forecWeather.list[j].temp.max) + "<i class=\"wi wi-degrees\"></i>";}
       function tMaxFahrenheit(k) { return calcTempF(forecWeather.list[k].temp.max) + "<i class=\"wi wi-degrees\"></i>";}
       function tMinCelsius(l) {return calcTempC(forecWeather.list[l].temp.min) + "<i class=\"wi wi-degrees\"></i>";}
       function tMinFahrenheit(m) { return calcTempF(forecWeather.list[m].temp.min) + "<i class=\"wi wi-degrees\"></i>";}
       //console.log(tMinCelsius(1));
 
-        //insert forecast data on the page
+        /***insert forecast data on the page***/
         //High
         $.each($('.tMax'), function(index, value) {
           $(this).html(tMaxCelsius(index));
@@ -89,7 +94,7 @@ $(document).ready(function() {
           $(this).html(forecDescr);
         });
 
-        //write the days of the week for the forecast
+        /***write the days of the week for the forecast***/
         var d = new Date();
         var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
@@ -99,7 +104,7 @@ $(document).ready(function() {
           $(this).html(days[nextDay.getDay()]);
         });
 
-        //Toggle between Celsius and Fahrenheit on click
+        /***Toggle between Celsius and Fahrenheit on click event***/
         unitSwitch(tCelsius, tFahrenheit, tMaxCelsius, tMaxFahrenheit, tMinCelsius, tMinFahrenheit);
 
       });
@@ -112,7 +117,7 @@ $(document).ready(function() {
 
   //WEATHER ICONS LIST AND FUNCTIONS
 
-  //list of weather icons
+  //list of weather iconsbackg
    var iconsList = {
     "01d" : "wi wi-day-sunny",
     "02d" : "wi wi-day-cloudy",
