c8a5e80a478d81b48f68067fcc2e8e4c9da6d36c
Erikson Oliveira <eriksonmagno6@hotmail.com>
2016-08-04 21:18:25 -0300

    Changes on temp chart

--- script_localWeather.js
+++ script_localWeather.js
@@ -64,119 +64,142 @@ $(document).ready(function() {
       function tMinFahrenheit(m) { return calcTempF(forecWeather.list[m].temp.min) + "<i class=\"wi wi-degrees\"></i>";}
       //console.log(tMinCelsius(1));
 
-        /***insert forecast data on the page***/
-        //High
-        $.each($('.tMax'), function(index, value) {
-          $(this).html(tMaxCelsius(index));
+      /***insert forecast data on the page***/
+      //High
+      $.each($('.tMax'), function(index, value) {
+        $(this).html(tMaxCelsius(index));
+      });
+
+      //unitForecSwitch('.tMax', '#tempC', '#tempF', tMaxCelsius, tMaxFahrenheit);
+
+      //Low
+      $.each($('.tMin'), function(index, value) {
+        $(this).html(tMinCelsius(index));
+      });
+
+      //weather condition
+      $.each($(".iconF"), function(index, value) {
+        var forecIcon = forecWeather.list[index].weather[0].icon;
+        $(this).addClass(iconsList[forecIcon]);
+      });
+
+      //Condition text
+      $.each($(".condF"), function(index, value) {
+        var forecDescr = forecWeather.list[index].weather[0].description;
+        //Make sure description fits the space
+        if ((forecDescr == "heavy intensity rain") || (forecDescr == "very heavy rain"))
+          {forecDescr = "heavy rain";}
+
+        forecDescr = uppercase(forecDescr);
+        $(this).html(forecDescr);
+      });
+
+
+      /****Create array to store temperature and date*****/
+      /*******************************/
+      var temps = [];
+
+      for ( i = 0; i < forecWeather.list.length; i++) {
+        var maT = calcTempC(forecWeather.list[i].temp.max),
+            miT = calcTempC(forecWeather.list[i].temp.min);
+        temps.push({
+          max: maT,
+          min: miT,
+          day: null
         });
-
-        //unitForecSwitch('.tMax', '#tempC', '#tempF', tMaxCelsius, tMaxFahrenheit);
-
-        //Low
-        $.each($('.tMin'), function(index, value) {
-          $(this).html(tMinCelsius(index));
-        });
-
-        //weather condition
-        $.each($(".iconF"), function(index, value) {
-          var forecIcon = forecWeather.list[index].weather[0].icon;
-          $(this).addClass(iconsList[forecIcon]);
-        });
-
-        //Condition text
-        $.each($(".condF"), function(index, value) {
-          var forecDescr = forecWeather.list[index].weather[0].description;
-          //Make sure description fits the space
-          if ((forecDescr == "heavy intensity rain") || (forecDescr == "very heavy rain"))
-            {forecDescr = "heavy rain";}
-
-          forecDescr = uppercase(forecDescr);
-          $(this).html(forecDescr);
-        });
+      }
 
 
-        /****CREATE ARRAY FOR TEMP*****/
-        /*******************************/
-        var temps = [];
+      /***Write the days of the week for the forecast***/
+      var d = new Date();
+      var week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
-        for ( i = 0; i < forecWeather.list.length; i++) {
-          var mat = calcTempC(forecWeather.list[i].temp.max),
-              mit = calcTempC(forecWeather.list[i].temp.min);
-          temps.push({
-            max: mat,
-            min: mit,
-            day: null
-          });
-        }
+      $.each($(".day"), function(index, value) {
+        var gettDay = new Date(d.getTime() + 86400 * 1000 * (index));
+        //console.log(nextDay);
+        var weekDay = week[gettDay.getDay()];
+        //var weekDayNum = gettDay.getDay();
+        var weekDayNum = gettDay.getTime();
+        $(this).html(weekDay);
 
+        //var date = gettDay.getDate() + "-" + gettDay.getMonth() + "-" + gettDay.getFullYear();
+        //var date = gettDay.toDateString();
+        //var patt = /(\w{3}) (\d{2}) (\d{4})/g;
+        //var result = date.match(patt);
+        //var a = gettDay.toJSON();
+        temps[index].day = weekDayNum ;
+        //temps[index].day = result;
 
-        /***Write the days of the week for the forecast***/
-        var d = new Date();
-        var week = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+        //console.log(temps);
+      });
 
-        $.each($(".day"), function(index, value) {
-          var gettDay = new Date(d.getTime() + 86400 * 1000 * (index));
-          //console.log(nextDay);
-          var weekDay = week[gettDay.getDay()];
-          var weekDayNum = gettDay.getDay();
-          $(this).html(weekDay);
-          temps[index].day = weekDayNum;
-        });
-
-        /******DRAW TEMP LINE CHART*******/
-        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      /******DRAW TEMPERATURE CHART*******/
+        //Set margins, height and width
+      var margin = {top: 20, right: 20, bottom: 50, left: 50},
           width = 400 - margin.left - margin.right,
           height = 300 - margin.top - margin.bottom;
+        //console.log(temps);
 
-        temps.forEach(function(d){
-          d.day = +d.day;
-          d.max = +d.max;
-          d.min = +d.min;
-        });
+      temps.forEach(function(d){
+        d.day = (new Date(d.day));
+        d.max = +d.max;
+        d.min = +d.min;
+      });
 
-        var x = d3.scale.linear()
-          .range([0, width]);
+      var timeFormat = d3.time.format('%Y-%m-%d');
 
-        var y = d3.scale.linear()
-          .range([height, 0]);
+      var x = d3.time.scale()
+        .range([0, width]);
 
-        x.domain(d3.extent(temps, function(d) { return d.day; }));
+      var y = d3.scale.linear()
+        .range([height, 0]);
 
-        var maxY = d3.max(temps, function(d) { return d.max; });
-        var minY = d3.min(temps, function(d) { return d.min - 5; });
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(2)
+        .tickFormat(timeFormat);;
 
-        y.domain([minY, maxY]);
+      x.domain(d3.extent(temps, function(d) { return new Date(d.day); }));
 
-        var graph = d3.select("#graph").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)   .append("g")
-          .attr("transform", "translate(50, 50)");
+      var maxY = d3.max(temps, function(d) { return d.max; });
+      var minY = d3.min(temps, function(d) { return d.min -2; });
 
-        var line = d3.svg.line()
-          .x(function(d) { return x(d.day); })
-          .y(function(d) { return y(d.max); });
+      y.domain([minY, maxY]);
 
-        var line2 = d3.svg.line()
-          .x(function(d) { return x(d.day) })
-          .y(function(d) { return y(d.min) });
+      var graph = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(50, 50)");
 
-        graph.append("path")
-          .data([temps])
-          .attr("class", "line")
-          .attr("d", line)
-          .attr("stroke", "#000")
-          .attr("stroke-width", 2.0)
-          .attr("fill", "none");
+      var line = d3.svg.line()
+        .interpolate("cardinal")
+        .x(function(d) { return x(d.day); })
+        .y(function(d) { return y(d.max); });
 
-        graph.append("path")
-          .data([temps])
-          .attr("class", "line")
-          .attr("d", line2)
-          .attr("stroke", "#838383")
-          .attr("stroke-width", 2.0)
-          .attr("fill", "none");
+      var line2 = d3.svg.line()
+        .interpolate("cardinal")
+        .x(function(d) { return x(d.day) })
+        .y(function(d) { return y(d.min) });
 
-        graph.selectAll("dot")
+      graph.append("path")
+        .data([temps])
+        .attr("class", "line")
+        .attr("d", line)
+        .attr("stroke", "#000")
+        .attr("stroke-width", 2.0)
+        .attr("fill", "none");
+
+      graph.append("path")
+        .data([temps])
+        .attr("class", "line")
+        .attr("d", line2)
+        .attr("stroke", "#838383")
+        .attr("stroke-width", 2.0)
+        .attr("fill", "none");
+
+      graph.selectAll("dot")
         .data([temps])
         .enter().append("g")
         .attr("class", "dot")
@@ -189,7 +212,7 @@ $(document).ready(function() {
         .attr("cx", function(d, i) { return x(d.day); })
         .attr("cy", function(d, i) { return y(d.max); });
 
-        graph.selectAll("dot")
+      graph.selectAll("dot")
         .data([temps])
         .enter().append("g")
         .attr("class", "dot")
@@ -201,6 +224,12 @@ $(document).ready(function() {
         //.attr("stroke", "#08c")
         .attr("cx", function(d, i) { return x(d.day); })
         .attr("cy", function(d, i) { return y(d.min); });
+
+      graph.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("stroke", "#000")
+        .call(xAxis);
 
         /***Call method to toggle between Celsius and Fahrenheit on click event***/
         unitSwitch(tCelsius, tFahrenheit, tMaxCelsius, tMaxFahrenheit, tMinCelsius, tMinFahrenheit);
