d57ca7c820406007d4f4eba6d472b6e3c5ad6d53
Erikson Oliveira <eriksonmagno6@hotmail.com>
2016-08-11 19:03:28 -0300

    Ajust Temperature chart to responsiveness

--- script_localWeather.js
+++ script_localWeather.js
@@ -118,16 +118,16 @@ $(document).ready(function() {
         var gettDay = new Date(d.getTime() + 86400 * 1000 * (index));
         //console.log(nextDay);
         var weekDay = week[gettDay.getDay()];
-        //var weekDayNum = gettDay.getDay();
-        var weekDayNum = gettDay.getTime();
+        var weekDayTime = gettDay.getTime();
         $(this).html(weekDay);
+        temps[index].day = weekDayTime;
 
-        //var date = gettDay.getDate() + "-" + gettDay.getMonth() + "-" + gettDay.getFullYear();
+        //var date = gettDay.getMonth() + " " + gettDay.getDate() + " " + gettDay.getFullYear();
         //var date = gettDay.toDateString();
         //var patt = /(\w{3}) (\d{2}) (\d{4})/g;
         //var result = date.match(patt);
         //var a = gettDay.toJSON();
-        temps[index].day = weekDayNum ;
+
         //temps[index].day = result;
 
         //console.log(temps);
@@ -135,10 +135,16 @@ $(document).ready(function() {
 
       /******DRAW TEMPERATURE CHART*******/
         //Set margins, height and width
-      var margin = {top: 20, right: 20, bottom: 50, left: 50},
-          width = 400 - margin.left - margin.right,
-          height = 300 - margin.top - margin.bottom;
-        //console.log(temps);
+      var width, height, margin = {}, chartWidth, chartHeight;
+
+      margin = {top: 20, right: 20, bottom: 50, left: 50};
+
+      chartWidth = 500 - margin.left - margin.right;
+      chartHeight = 200 - margin.top - margin.bottom;
+
+      width = chartWidth + margin.left + margin.right;
+      height = chartHeight + margin.top + margin.bottom;
+
 
       temps.forEach(function(d){
         d.day = (new Date(d.day));
@@ -146,19 +152,21 @@ $(document).ready(function() {
         d.min = +d.min;
       });
 
-      var timeFormat = d3.time.format('%Y-%m-%d');
+      var timeFormat = d3.time.format('%a %d');
 
       var x = d3.time.scale()
-        .range([0, width]);
+        .range([0, chartWidth]);
 
       var y = d3.scale.linear()
-        .range([height, 0]);
+        .range([chartHeight, 0]);
 
       var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom")
-        .ticks(2)
-        .tickFormat(timeFormat);;
+        .ticks(d3.time.hour, 12)
+        .tickPadding(5)
+        .outerTickSize(0)
+        .tickFormat(timeFormat);
 
       x.domain(d3.extent(temps, function(d) { return new Date(d.day); }));
 
@@ -168,27 +176,30 @@ $(document).ready(function() {
       y.domain([minY, maxY]);
 
       var graph = d3.select("#graph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        //.attr("width", width + margin.left + margin.right)
+        //.attr("height", height + margin.top + margin.bottom)
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", "0 0 " + width + " " + height)
+        .attr("preserveAspectRatio", "xMinYMid meet")
         .append("g")
-        .attr("transform", "translate(50, 50)");
+        .attr("transform", "translate(20, 30)");
 
       var line = d3.svg.line()
         .interpolate("cardinal")
-        .x(function(d) { return x(d.day); })
+        .x(function(d) { return x(new Date(d.day)); })
         .y(function(d) { return y(d.max); });
 
       var line2 = d3.svg.line()
         .interpolate("cardinal")
         .x(function(d) { return x(d.day) })
         .y(function(d) { return y(d.min) });
-
       graph.append("path")
         .data([temps])
         .attr("class", "line")
         .attr("d", line)
         .attr("stroke", "#000")
-        .attr("stroke-width", 2.0)
+        .attr("stroke-width", 1.5)
         .attr("fill", "none");
 
       graph.append("path")
@@ -196,7 +207,7 @@ $(document).ready(function() {
         .attr("class", "line")
         .attr("d", line2)
         .attr("stroke", "#838383")
-        .attr("stroke-width", 2.0)
+        .attr("stroke-width", 1.5)
         .attr("fill", "none");
 
       graph.selectAll("dot")
@@ -206,9 +217,8 @@ $(document).ready(function() {
         .selectAll("circle")
         .data(function(d) { return d; })
         .enter().append("circle")
-        .attr("r", 3.0)
+        .attr("r", 3.5)
         .attr("fill", "#000")
-        //.attr("stroke", "#a22")
         .attr("cx", function(d, i) { return x(d.day); })
         .attr("cy", function(d, i) { return y(d.max); });
 
@@ -219,17 +229,19 @@ $(document).ready(function() {
         .selectAll("circle")
         .data(function(d) { return d; })
         .enter().append("circle")
-        .attr("r", 3.0)
+        .attr("r", 3.5)
         .attr("fill", "#838383")
         //.attr("stroke", "#08c")
         .attr("cx", function(d, i) { return x(d.day); })
         .attr("cy", function(d, i) { return y(d.min); });
 
-      graph.append("g")
+      /*graph.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .attr("stroke", "#000")
         .call(xAxis);
+        */
+
 
         /***Call method to toggle between Celsius and Fahrenheit on click event***/
         unitSwitch(tCelsius, tFahrenheit, tMaxCelsius, tMaxFahrenheit, tMinCelsius, tMinFahrenheit);
